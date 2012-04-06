13f8c4bd43c231dc86317e3bae8d3cf52c7e605c
Jack Kelly <jack-list@xlk.org.uk>
2012-04-06 14:18:32 +0100

    further comments

--- PA3/displayWord.m
+++ PA3/displayWord.m
@@ -18,12 +18,17 @@ colormap([1 1 1; 0 0 0]); % define a monochrome colour map
 
 % Loop through each character in the word
 for charIndex = 1:numChars
+    % Create enough subplots for each character and select
+    % the required subplot for the current character.
     subplot(1, numChars, charIndex);
-    
+
+    % Display the character bitmap as an index,
+    % using the colormap defined above.
     image( allWords{wordIndex}(charIndex).img + 1); % we need the +1
     % because the .img matrix contains values which are 0 or 1;
     % but colour maps are indexed starting at 1.
-    
+
+    % Format the image
     axis equal;
     set(gca, 'XLim', [0.5 8.5], 'YLim', [0.5 16.5], 'XTick', [], 'YTick', []);
     title( char( allWords{wordIndex}(charIndex).groundTruth + 'a' - 1 ) );
